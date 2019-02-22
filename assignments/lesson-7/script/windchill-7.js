77e476a5bea53a06da5b3b89b1b6a16ef67ec621
chelsiaj <Enter your email address>
2019-02-21 22:12:50 -0800

    a little fix with the equation

--- assignments/lesson-7/script/windchill-7.js
+++ assignments/lesson-7/script/windchill-7.js
@@ -4,14 +4,15 @@ var speed = parseInt(document.getElementById('WindSpeed').innerHTML);
 var wind_chill;
 var first_val = 35.74;
 var second_val = 0.6215 * tempF;
-var third_val = 35.75 * speed;
-var fourth_val = Math.pow(third_val, .16);
+var third_val = 35.75;
+var fourth_val = Math.pow(speed, .16);
 var fifth_val = 0.4275 * tempF;
 var sixth_val = Math.pow(speed, 0.16);
 var seventh_val = fifth_val * sixth_val;
+var eighth_val = third_val * fourth_val;
 	
 
-	wind_chill = first_val + second_val - fourth_val + seventh_val;
+	wind_chill = first_val + second_val - eighth_val + seventh_val;
 	
 
 document.getElementById("wind_chill").innerHTML = Math.round(wind_chill);
