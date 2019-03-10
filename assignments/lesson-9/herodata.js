244a60edad374a225a12f799a43ff76cd4d59c1b
chelsiaj <Enter your email address>
2019-03-09 21:46:21 -0800

    homepage

--- assignments/lesson-9/herodata.js
+++ assignments/lesson-9/herodata.js
@@ -10,22 +10,13 @@ request.onload = function() {
     showHeroes(superHeroes);
   }
 
-  function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['squadName'];
-    header.appendChild(myH1);
-  
-    var myPara = document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-    header.appendChild(myPara);
-  }
+
 
   function showHeroes(jsonObj) {
     var heroes = jsonObj['members'];
         
     for (var i = 0; i < heroes.length; i++) {
       var myArticle = document.createElement('article');
-      var myH2 = document.createElement('h2');
       var myPara1 = document.createElement('p');
       var myPara2 = document.createElement('p');
       var myPara3 = document.createElement('p');
@@ -43,11 +34,10 @@ request.onload = function() {
         myList.appendChild(listItem);
       }
   
-      myArticle.appendChild(myH2);
+   
       myArticle.appendChild(myPara1);
       myArticle.appendChild(myPara2);
       myArticle.appendChild(myPara3);
-      myArticle.appendChild(myList);
   
       section.appendChild(myArticle);
     }
